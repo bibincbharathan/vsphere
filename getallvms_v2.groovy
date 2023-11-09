cadeebfd9ebf0b0b553d90af62715ebbf22a963d
bbharathan <bbharathan@vmware.com>
2023-11-09 22:05:05 +0530

    bibin

--- getallvms_v2.groovy
+++ getallvms_v2.groovy
@@ -4,9 +4,9 @@ pipeline {
     parameters {
         string(name: 'PYTHON_PATH', defaultValue: '/Users/bbharathan/opt/anaconda3/bin/python', description: 'Path to Python executable')
         string(name: 'SCRIPT_PATH', defaultValue: 'getallvms_v2.py', description: 'Path to Python script')
-        string(name: 'HOST', defaultValue: '10.187.96.82', description: 'VC')
+        string(name: 'HOST', defaultValue: 'wdc-haas-vc02.oc.vmware.com', description: 'VC')
         string(name: 'USER', defaultValue: 'administrator@vsphere.local', description: 'USER')
-        string(name: 'PASSWORD', defaultValue: 'Artvm@123', description: 'Password')
+        string(name: 'PASSWORD', defaultValue: 'SvL9n9123!', description: 'Password')
 
     }
 
